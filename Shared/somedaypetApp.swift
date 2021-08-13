8937c89decf9059fe260dc4fe13b9c5fcfb94f60
irinaperesypkina <iperesypkina9@gmail.com>
2021-08-13 14:53:50 +0300

    Initial Commit

---
+++ Shared/somedaypetApp.swift
@@ -0,0 +1,17 @@
+//
+//  somedaypetApp.swift
+//  Shared
+//
+//  Created by Ирина Пересыпкина on 13.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct somedaypetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
